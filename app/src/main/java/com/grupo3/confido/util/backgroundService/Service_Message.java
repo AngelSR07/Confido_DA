8ecf7930588de753380f756f6a593f33c43e860d
AngelSR07 <angelshapiamarivera@gmail.com>
2022-06-17 15:51:03 -0500

    Initial commit - Shapiama Rivera, Angel Alfredo

---
+++ app/src/main/java/com/grupo3/confido/util/backgroundService/Service_Message.java
@@ -0,0 +1,132 @@
+package com.grupo3.confido.util.backgroundService;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.media.VolumeProviderCompat;
+
+import com.grupo3.confido.util.sendMessage.Contact;
+import com.grupo3.confido.util.sendMessage.RxJava;
+
+public class Service_Message extends Service {
+
+    //Atributos
+    private MediaSessionCompat mediaSessionCompat;
+    private int cont;
+    private BroadcastReceiver myBroadcast;
+    private IntentFilter filter;
+    private RxJava rxJava;
+
+
+    //Manejamos todos los componentes al inicio una vez cargada la clase
+    @Override
+    public void onCreate() {
+        Toast.makeText(this,"El servicio ha sido creado", Toast.LENGTH_SHORT).show();
+
+        mediaSessionCompat = new MediaSessionCompat(this,"Service_Message");
+
+        mediaSessionCompat.setPlaybackState(new PlaybackStateCompat.Builder().setState(PlaybackStateCompat.STATE_PLAYING,0,0).build());
+
+        rxJava = new RxJava();
+        listContact();
+    }
+
+
+
+
+    //Permite iniciar el servicio en segundo plano
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this,"Servicio inciado " + startId, Toast.LENGTH_SHORT).show();
+
+        filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+
+        myBroadcast = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                //Si la pantalla se encuentra suspendida o apagada
+                if(intent.getAction().equals(Intent.ACTION_SCREEN_OFF)){
+                    VolumeProviderCompat myVolumenProvider = new VolumeProviderCompat(VolumeProviderCompat.VOLUME_CONTROL_RELATIVE,50,50) {
+                        @Override
+                        public void onAdjustVolume(int direction) {
+                            //Si presiona el boton "Volumen +"
+                            if(direction == 1){
+                                cont++;
+
+                                if(cont == 5){
+                                    rxJava.startEvent();//Iniciamos el proceso asincrono
+                                    cont = 0;
+                                }
+                            }
+                        }
+                    };
+
+                    mediaSessionCompat.setPlaybackToRemote(myVolumenProvider);
+                    mediaSessionCompat.setActive(true);
+
+                } else if(intent.getAction().equals(Intent.ACTION_SCREEN_ON)){
+                    //Toast.makeText(Service_Message.this,"Se prendio la pantalla en 2do plano :D", Toast.LENGTH_SHORT).show();
+                    mediaSessionCompat.release();
+                }
+
+            }
+        };
+
+        registerReceiver(myBroadcast,filter);
+
+        return START_STICKY;//Bandera que indica que el servicio se está ejecuntado (1).
+    }
+
+
+
+    //Metodo para añadir los contactos a notificar
+    private void listContact(){
+
+        Contact c1 = new Contact("Angel",11111);
+        Contact c2 = new Contact("Gaby",22222);
+        Contact c3 = new Contact("Brenda",33333);
+        Contact c4 = new Contact("Tracy",44444);
+        Contact c5 = new Contact("Mery",55555);
+
+        rxJava.addContacts(c1);
+        rxJava.addContacts(c2);
+        rxJava.addContacts(c3);
+        rxJava.addContacts(c4);
+        rxJava.addContacts(c5);
+    }
+
+
+
+
+    //Permite detener el servicio en 2do plano
+    @Override
+    public void onDestroy() {
+
+        Toast.makeText(this,"Servicio detenido", Toast.LENGTH_SHORT).show();
+
+        rxJava.destroyDisposable();
+        rxJava = null;//Limpiamos la lista de contactos
+
+        unregisterReceiver(myBroadcast);
+        mediaSessionCompat.release();
+
+        super.onDestroy();
+    }
+
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/grupo3/confido/util/sendMessage/RxJava.java
@@ -0,0 +1,90 @@
+package com.grupo3.confido.util.sendMessage;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class RxJava {
+
+    private Disposable disposable;
+    private final List<Contact> contacts;
+
+
+    public RxJava() {
+        contacts = new ArrayList<>();
+    }
+
+
+
+    public void addContacts(Contact c){
+        contacts.add(c);
+    }
+
+
+
+    public int sizeListContacts(){
+        return contacts.size();
+    }
+
+
+
+    public void startEvent(){
+        Observable<Contact> observable = Observable.fromIterable(contacts);
+
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(createObserver());
+    }
+
+
+
+    private Observer<Contact> createObserver(){
+        return new Observer<Contact>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                disposable = d;
+            }
+
+            @Override
+            public void onNext(Contact c) {
+                sendMessage(c);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {}
+
+            @Override
+            public void onComplete() {}
+        };
+    }
+
+
+
+    private void sendMessage(Contact c){
+        //Implementar lógica de enviar mensaje
+
+        Log.e("Mensaje","El mensaje se ha enviado al número: " + c.getNumContact());
+        getLocation();
+    }
+
+
+    private void getLocation(){
+        //Implementar logica de obtener ubicación
+
+        Log.i("Ubicación","La ubicación ha sido localizada");
+    }
+
+
+
+    public void destroyDisposable(){
+        disposable.dispose();
+    }
+}
